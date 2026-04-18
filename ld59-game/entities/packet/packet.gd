a2b3b5a06851231aa499bac1b50db8d10b39a181
Ian Risch <ian.risch@gmail.com>
2026-04-18 21:10:29 +0200

    Change Growth to start with 1 and increase by 0.1 every time

--- ld59-game/entities/packet/packet.gd
+++ ld59-game/entities/packet/packet.gd
@@ -3,9 +3,10 @@ extends RigidBody2D
 class_name Packet
 
 @export_category("Properties")
-@export var ttl: int = 5;
-@export var payload: int = 1;
-@export var degradation: int;
+@export var ttl: int = 5
+@export var payload: int = 1
+@export var degradation: int
+@export var growthModifier := 0.1
 
 func _ready() -> void:
 	body_entered.connect(onCollision)
@@ -18,7 +19,7 @@ func onCollision(body: Node2D) -> void:
 
 func increasePayload() -> void:
 	payload += 1
-	var newScale := (Vector2.ONE * payload)
+	var newScale := Vector2.ONE * (1 + (payload-1) * growthModifier)
 	for child in get_children():
 		if child is Node2D:
 			child.scale = newScale
