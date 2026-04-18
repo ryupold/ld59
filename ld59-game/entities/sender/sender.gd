0c3ce460cf26a4b3e0c5cf1d06e94ae1ea994e37
Michael Scherbakow <ryupold@gmail.com>
2026-04-18 17:03:33 +0200

    add collision shapes to walls

--- ld59-game/entities/sender/sender.gd
+++ ld59-game/entities/sender/sender.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @export var packetScene: PackedScene = preload("res://entities/packet/packet.tscn")
+@export var packetParent: Node2D
+@export var spawnDistance: float
 
 func _on_packet_creation_timer_timeout():
-	var packet := packetScene.instantiate()
-	packet.position = position
-	add_child(packet)
+	var packet : Packet = packetScene.instantiate()
+	packet.global_position = global_position + spawnDistance * Vector2.from_angle(randf() * 2 * PI)
+	packetParent.add_child(packet)
