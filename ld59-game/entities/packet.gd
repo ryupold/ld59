86718ff79354fc2f7478e747a89a5531e09e1cfb
Ian Risch <ian.risch@gmail.com>
2026-04-18 14:18:39 +0200

    Add Packet

---
+++ ld59-game/entities/packet.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+class_name Packet
+
+@export_category("Properties")
+@export var time_to_live: int = 3;
+@export var payload: int = 1;
+@export var degradation: int;
+@export var speed: Vector2;
