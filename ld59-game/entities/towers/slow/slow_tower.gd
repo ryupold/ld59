338f380a148655e354150b1681b035be10db6519
Michael Scherbakow <ryupold@gmail.com>
2026-04-19 00:02:28 +0200

    add parameters to slow tower
    also do not slow down particle below a certain velocity

--- ld59-game/entities/towers/slow/slow_tower.gd
+++ ld59-game/entities/towers/slow/slow_tower.gd
@@ -1,7 +1,14 @@
 extends Tower
 
+@export var slowDownFactor: float = 0.5
+@export var minParticleSpeed: float = 100
+
 func _physics_process(delta: float) -> void:
 	for packet in packetsColliding:
-		var velocity := packet.linear_velocity - (packet.linear_velocity * 2 * delta)
+		if packet.linear_velocity.length() < minParticleSpeed:
+			continue
+			
+		var velocity := packet.linear_velocity - (packet.linear_velocity * slowDownFactor * delta)
+		if velocity.length() < minParticleSpeed:
+			velocity = velocity.normalized() * minParticleSpeed
 		packet.set_axis_velocity(velocity)
-	return
