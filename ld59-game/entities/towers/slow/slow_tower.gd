81e031c55db3e8adfbb68dc40c4be2d282335060
Ian Risch <ian.risch@gmail.com>
2026-04-18 22:06:49 +0200

    Add Slow Tower

---
+++ ld59-game/entities/towers/slow/slow_tower.gd
@@ -0,0 +1,7 @@
+extends Tower
+
+func _physics_process(delta: float) -> void:
+	for packet in packetsColliding:
+		var velocity := packet.linear_velocity - (packet.linear_velocity * 2 * delta)
+		packet.set_axis_velocity(velocity)
+	return
