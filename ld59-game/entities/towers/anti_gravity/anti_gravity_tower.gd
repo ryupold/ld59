625809735d206d73c4dfe553d790dbdb853a164c
Ian Risch <ian.risch@gmail.com>
2026-04-18 18:09:39 +0200

    Apply Gravity in physicsprocess

--- ld59-game/entities/towers/anti_gravity/anti_gravity_tower.gd
+++ ld59-game/entities/towers/anti_gravity/anti_gravity_tower.gd
@@ -1,6 +1,6 @@
 extends Tower
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	for packet in packetsColliding:
 		var offset := global_position.direction_to(packet.global_position)
 		packet.apply_impulse(offset * 10)
--- ld59-game/entities/towers/gravity/gravity_tower.gd
+++ ld59-game/entities/towers/gravity/gravity_tower.gd
@@ -1,24 +1,7 @@
 extends Tower
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	for packet in packetsColliding:
 		var offset := packet.global_position.direction_to(global_position)
 		packet.apply_impulse(offset.normalized() * 10)
-	return
-
-func addCompute(body: Node2D) -> void:
-	if body is Packet:
-		var packet := body as Packet
-		packetsColliding.append(packet)
-
-	return
-
-
-func removeCompute(body: Node2D) -> void:
-	if body is Packet:
-		var packet := body as Packet
-		var pos := packetsColliding.find(packet)
-		if pos == -1:
-			return
-		packetsColliding.remove_at(pos);
 	return
