7d940ae341ab576d20bddf555fd536f58fa74432
Ian Risch <ian.risch@gmail.com>
2026-04-18 21:04:49 +0200

    Fix PayloadTower

--- ld59-game/entities/packet/packet.gd
+++ ld59-game/entities/packet/packet.gd
@@ -7,16 +7,18 @@ class_name Packet
 @export var payload: int = 1;
 @export var degradation: int;
 
-func _ready():
+func _ready() -> void:
 	body_entered.connect(onCollision)
 
-func onCollision(body: Node2D):
+func onCollision(body: Node2D) -> void:
 	ttl -= 1
 	if ttl <= 0:
 		GameState.onPacketLost.emit()
 		queue_free()
 
-func increasePayload():
-	print("kfnsdfnsdfkjn")
+func increasePayload() -> void:
 	payload += 1
-	set_scale(Vector2.ONE * payload )
+	var newScale := (Vector2.ONE * payload)
+	for child in get_children():
+		if child is Node2D:
+			child.scale = newScale
--- ld59-game/entities/towers/payload/payload_tower.gd
+++ ld59-game/entities/towers/payload/payload_tower.gd
@@ -1,21 +1,13 @@
 extends Tower
 
-var tween: Tween
-
 func _ready() -> void:
+	super._ready()
 	usePacketList = false
 	onPacketEnter.connect(func(p:Packet): p.increasePayload())
-	onPacketEnter.connect(func(p:Packet): print("enter"))
-	var collsioonshape: CollisionShape2D = collisionArea.get_child(0)
-	var shape: CircleShape2D = collsioonshape.shape
-	tween = create_tween().set_loops()
-	tween.tween_property(shape, "radius", 600.0, 1)
+
+	collisionArea.scale = Vector2.ZERO
+	var tween := create_tween().set_loops()
+	tween.tween_property(collisionArea, "scale", Vector2.ONE, 1)
 	tween.tween_interval(0.5)
-	tween.tween_property(shape, "radius", 0.0, 0)
+	tween.tween_property(collisionArea, "scale", Vector2.ZERO, 0)
 	tween.tween_interval(1)
-
-
-func _physics_process(delta):
-	var overlapping := collisionArea.get_overlapping_bodies()
-	if overlapping.size() > 0:
-		(overlapping[0] as Packet).increasePayload()
