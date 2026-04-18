cc23b49985003f0c6da8a36d1f56f31cb4cc4b4a
Michael Scherbakow <ryupold@gmail.com>
2026-04-18 16:46:12 +0200

    update viewport and fix scale

--- ld59-game/entities/sender/sender.gd
+++ ld59-game/entities/sender/sender.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@export var packetScene: PackedScene = preload("res://entities/packet/packet.tscn")
+
 func _on_packet_creation_timer_timeout():
-	var packet := Packet.new()
+	var packet := packetScene.instantiate()
 	packet.position = position
 	add_child(packet)
