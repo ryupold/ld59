223403ecd81959d7cb026e1204c9d6842be9489d
Ian Risch <ian.risch@gmail.com>
2026-04-18 21:45:50 +0200

    Add Relay Tower

---
+++ ld59-game/entities/towers/relay/relay_tower.gd
@@ -0,0 +1,18 @@
+extends Tower
+
+func _ready() -> void:
+	collisionArea.body_entered.connect(addCompute)
+
+	collisionArea.scale = Vector2.ZERO
+	var tween := create_tween().set_loops()
+	tween.tween_property(collisionArea, "scale", Vector2.ONE, 1)
+	tween.tween_interval(0.5)
+	tween.tween_property(collisionArea, "scale", Vector2.ZERO, 0)
+	tween.tween_interval(1)
+
+
+func addCompute(body: Node2D) -> void:
+	if body is Packet:
+		var packet := body as Packet
+		packet.increaseTtl()
+	return
