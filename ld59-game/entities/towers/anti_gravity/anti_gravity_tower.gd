a123694dc8311768b051d24c7bc3a65650f29534
Michael Scherbakow <ryupold@gmail.com>
2026-04-19 14:43:18 +0200

    add antigravity tower to resources

--- ld59-game/entities/towers/anti_gravity/anti_gravity_tower.gd
+++ ld59-game/entities/towers/anti_gravity/anti_gravity_tower.gd
@@ -1,9 +1,8 @@
 extends Tower
 
-@export var antiGravity: float = 9.81
+@export var antiGravity: float = -1500
 
 func doEffect(delta: float) -> void:
 	for packet in packetsColliding:
 		var offset := global_position.direction_to(packet.global_position)
-		packet.apply_impulse(offset * antiGravity)
-	return
+		packet.apply_force(offset.normalized() * antiGravity)
