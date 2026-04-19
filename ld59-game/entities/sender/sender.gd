42cda5787e6aab3c95a1fbad8e12bc6f75826184
Michael Scherbakow <ryupold@gmail.com>
2026-04-19 02:05:43 +0200

    let sender spray packets in 90degree arc

--- ld59-game/entities/sender/sender.gd
+++ ld59-game/entities/sender/sender.gd
@@ -4,13 +4,31 @@ extends Node2D
 @export var packetParent: Node2D
 @export var spawnDistance: float
 @export var spawnInterval: float = 1.0
+@export var spawnImpulse: float = 1000
+@export var spawnAngleMin: float = 0
+@export var spawnAngleMax: float = PI/2
+@export var spawnAngleRotationSpeed: float = 1
+var _spawnAngle: float
 
 func _ready():
+	_spawnAngle = spawnAngleMin
 	$PacketCreationTimer.wait_time = spawnInterval
 	$PacketCreationTimer.timeout.connect(createPacket)
 
+func _physics_process(delta):
+	var nextAngle := wrapf(_spawnAngle + spawnAngleRotationSpeed * delta, spawnAngleMin, spawnAngleMax)
+	if spawnAngleRotationSpeed > 0 and nextAngle < _spawnAngle:
+		spawnAngleRotationSpeed *= -1
+		_spawnAngle = spawnAngleMax
+	elif spawnAngleRotationSpeed < 0 and nextAngle > _spawnAngle:
+		spawnAngleRotationSpeed *= -1
+		_spawnAngle = spawnAngleMin
+	else:
+		_spawnAngle = nextAngle
+
 func createPacket():
 	var packet : Packet = packetScene.instantiate()
+	var position := global_position + Vector2.from_angle(_spawnAngle) * spawnDistance
 	packet.global_position = global_position
-	packet.apply_impulse(Vector2.from_angle(randf_range(0, PI)) * 1000)
+	packet.apply_impulse(Vector2.from_angle(_spawnAngle) * spawnImpulse)
 	packetParent.add_child(packet)
