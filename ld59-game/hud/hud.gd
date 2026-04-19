f57e7d7c52ec5f7593cde237b1cafced51060f89
Ian Risch <ian.risch@gmail.com>
2026-04-19 22:33:56 +0200

    Add Level Selector Button

--- ld59-game/hud/hud.gd
+++ ld59-game/hud/hud.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
-@export var button :Button
+@export var button: Button
+@export var buttonSelector: Button
+@export var levelSelector: LevelSelectorCanvasItem
 
 func _ready() -> void:
 	button.pressed.connect(GameState.restartLevel)
+	buttonSelector.pressed.connect(levelSelector.toggleSelector)
--- ld59-game/hud/level_selector/level_selector.gd
+++ ld59-game/hud/level_selector/level_selector.gd
@@ -1,13 +1,25 @@
-extends VBoxContainer
+class_name LevelSelectorCanvasItem extends VBoxContainer
 
 @export var levelCanvasItem: PackedScene = preload("res://hud/level_selector/level_canvas_item.tscn")
 @export var buttonParent: Control
 
 func _ready() -> void:
+	updateLevelSelect()
+
+func toggleSelector() -> void:
+	updateLevelSelect()
+	if visible:
+		hide()
+	else:
+		show()
+
+func updateLevelSelect() -> void:
 	for child in buttonParent.get_children():
 		child.queue_free()
-
-	for ressource: LevelResource in GameState.levels:
+	for i in GameState.levels.size():
+		var ressource: LevelResource = GameState.levels[i]
+		var prev: LevelResource = GameState.levels[i-1]
 		var canvasItem: LevelCanvasItem = levelCanvasItem.instantiate()
 		canvasItem.resource = ressource
+		canvasItem.disabled = false if i-1 == -1 else !prev.completed
 		buttonParent.add_child(canvasItem)
